837d8748991ea65bd02cdbbe2b386e1c16749bee
itsmefminsaf <itsmefminsaf@gmail.com>
2025-09-28 20:57:26 +0530

    Enhance styling and structure of the application
    - Imported Kendo UI default theme for consistent styling.
    - Defined CSS variables for color management.
    - Updated body styles to include a radial gradient background and centered layout.
    - Created a new class for a background clip effect.
    - Styled headings with custom typography and spacing.
    - Added a button component with hover and active states.
    - Refactored App component to utilize Kendo UI Typography and Button components for better UI consistency.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,23 @@
 "use client";
 
+import { Button } from "@progress/kendo-react-buttons";
+import { Typography } from "@progress/kendo-react-common";
+
 const App = () => {
-	return <h1 className="app-name">Resumate</h1>;
+	return (
+		<>
+			<div className="bg-clip"></div>
+			<Typography.h1 className="app-name">
+				WELCOME TO
+				<span>
+					<br />
+					RESUMATE
+				</span>
+			</Typography.h1>
+			<Typography.h4>An AI powered resume building application</Typography.h4>
+			<Button>Create now..!!!</Button>
+		</>
+	);
 };
 
 export default App;
